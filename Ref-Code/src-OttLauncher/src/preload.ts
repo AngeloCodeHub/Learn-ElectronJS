96d69c028e33ea1693bbdec64d2a40e0b20ce520
Angelo <a0920217700@gmail.com>
2026-09-21 06:10:56 +0800

    ottlauncher移出

--- Ref-Code/src-OttLauncher/src/preload.ts
+++
@@ -1,8 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-const api = {
-	startNetflixLogin: () => ipcRenderer.invoke('netflix:login'),
-	launchDisney: () => ipcRenderer.invoke('disney:launch'),
-};
-
-contextBridge.exposeInMainWorld('electronAPI', api);
